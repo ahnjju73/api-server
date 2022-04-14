0981219f55f09fd6ff0eec40c55788b33fcd674d
ijunc2 <ijunc2@gmail.com>
2022-04-14 11:46:59 +0900

    계약서 목록 조회시, 조회Condition 조건 추가 * lease_id, contract_type (계약형태)

--- src/main/java/helmet/bikelab/apiserver/objects/requests/RequestListDto.java
+++ src/main/java/helmet/bikelab/apiserver/objects/requests/RequestListDto.java
@@ -2,12 +2,13 @@ package helmet.bikelab.apiserver.objects.requests;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import helmet.bikelab.apiserver.services.internal.OriginObject;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class RequestListDto {
+public class RequestListDto extends OriginObject {
     private String nextToken;
 }
