d6767272cbb1c6fea4fb46f7ff3afc10ea8855bf
ijunc2 <ijunc2@gmail.com>
2022-04-19 13:32:57 +0900

    부품목록 조회 api 등록

--- src/main/java/helmet/bikelab/apiserver/repositories/PartsCodesRepository.java
+++ src/main/java/helmet/bikelab/apiserver/repositories/PartsCodesRepository.java
@@ -1,6 +1,8 @@
 package helmet.bikelab.apiserver.repositories;
 
 import helmet.bikelab.apiserver.domain.bike.PartsCodes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +10,5 @@ import java.util.List;
 public interface PartsCodesRepository extends JpaRepository<PartsCodes, Integer> {
     PartsCodes findByPartsCodeNo(Integer partCodeNo);
     List<PartsCodes> findByPartsTypeNo(Integer partsTypeNo);
+    Page<PartsCodes> findAllByPartsNameContainingAndPartsType_PartsTypeContaining(String partsName, String partsType, Pageable pageable);
 }
