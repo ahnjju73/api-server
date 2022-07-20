cb4d1d53fb7c3c90e48543f37d36e24917b0b496
on_us_hong <on_us_hong@on-us-hongs-MacBook-Pro.local>
2022-07-20 17:31:56 +0900

    feature/공지사항_개발api

---
+++ src/main/java/helmet/bikelab/apiserver/domain/Notifications.java
@@ -0,0 +1,57 @@
+package helmet.bikelab.apiserver.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import helmet.bikelab.apiserver.domain.bike.ImageVo;
+import helmet.bikelab.apiserver.domain.types.converters.ImageVoConverter;
+import helmet.bikelab.apiserver.utils.keys.SESSION;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "notifications")
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class Notifications {
+
+    @Id
+    @Column(name = "notification_no")
+    private Integer notificationNo;
+
+    @Column(name = "title", length = 255)
+    private String title;
+
+    @Column(name = "content", columnDefinition = "MEDIUMTEXT")
+    private String content;
+
+    @JsonIgnore
+    @Column(name = "images", columnDefinition = "json")
+    @Convert(converter = ImageVoConverter.class)
+    private List<ImageVo> imageList;
+
+    @Column(name = "images", columnDefinition = "json", insertable = false, updatable = false)
+    private String images;
+
+    @JsonIgnore
+    @Column(name = "attachments", columnDefinition = "json")
+    @Convert(converter = ImageVoConverter.class)
+    private List<ImageVo> attachmentList;
+
+    @Column(name = "attachments", columnDefinition = "json", insertable = false, updatable = false)
+    private String attachments;
+
+    @Column(name = "start_time", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime endTime;
+
+}
