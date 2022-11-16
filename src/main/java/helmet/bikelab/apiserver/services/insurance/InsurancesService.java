cae70d2a5edee261dc5cba8635272305af8ca3af
on_us_hong <on_us_hong@on-us-hongs-MacBook-Pro.local>
2022-11-16 18:47:42 +0900

    riderInsNo

--- src/main/java/helmet/bikelab/apiserver/services/insurance/InsurancesService.java
+++ src/main/java/helmet/bikelab/apiserver/services/insurance/InsurancesService.java
@@ -181,7 +181,7 @@ public class InsurancesService extends SessService {
 
     public BikeSessionRequest fetchRiderInsurances(BikeSessionRequest request) {
         FetchRiderInsuranceRequest fetchRiderInsuranceRequest = map(request.getParam(), FetchRiderInsuranceRequest.class);
-        Pageable pageable = PageRequest.of(fetchRiderInsuranceRequest.getPage(), fetchRiderInsuranceRequest.getSize(), Sort.by("rider_ins_no").descending());
+        Pageable pageable = PageRequest.of(fetchRiderInsuranceRequest.getPage(), fetchRiderInsuranceRequest.getSize(), Sort.by("riderInsNo").descending());
 
         if (bePresent(fetchRiderInsuranceRequest.getRiderName()) && bePresent(fetchRiderInsuranceRequest.getStatus())) {
             Page<RiderInsurances> allByRiderInsurancesDtl_riderInfoDto_riderNameContaining = riderInsuranceRepository.findAllByRiderInsurancesDtl_RiderNameContainingAndRiderInsurancesDtl_RiderInsuranceStatus(fetchRiderInsuranceRequest.getRiderName(), RiderInsuranceStatus.getStatus(fetchRiderInsuranceRequest.getStatus()), pageable);
