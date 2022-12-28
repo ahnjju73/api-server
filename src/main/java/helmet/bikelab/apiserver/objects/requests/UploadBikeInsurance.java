3cada914be1675d3b09d0ec39e361f925eb95077
ijunc2 <ijunc2@gmail.com>
2022-12-28 11:43:10 +0900

    updated

--- src/main/java/helmet/bikelab/apiserver/objects/requests/UploadBikeInsurance.java
+++ src/main/java/helmet/bikelab/apiserver/objects/requests/UploadBikeInsurance.java
@@ -18,19 +18,19 @@ public class UploadBikeInsurance extends BikeInsuranceInfo {
     private Boolean paid = false;
 
     public void checkValidation(StringBuilder errorText){
-        if(!bePresent(getGrade())) errorText.append("등급을 선택해주세요.\n");
-        if(!bePresent(getStockNumber())) errorText.append("증권번호를 입력해주세요.\n");
-        if(!bePresent(getFee())) errorText.append("보험료를 입력해주세요.\n");
-        if(!bePresent(getBikeInsuranceType())) errorText.append("보험용도를 입력해주세요.\n");
-        if(!bePresent(getType())) errorText.append("보험형태를 입력해주세요.\n");
-        if(!bePresent(getCompanyName())) errorText.append("보험사를 입력해주세요.\n");
-        if(!bePresent(getAge())) errorText.append("나이를 입력해주세요.\n");
-        if(!bePresent(getLiabilityMan())) errorText.append("대인금액을 입력해주세요.\n");
-        if(!bePresent(getLiabilityCar())) errorText.append("대물금액을 입력해주세요.\n");
-        if(!bePresent(getLiabilityMan2())) errorText.append("대인2금액을 입력해주세요.\n");
-        if(!bePresent(getSelfCoverMan())) errorText.append("자손금액을 입력해주세요.\n");
-        if(!bePresent(getSelfCoverCar())) errorText.append("자차금액을 입력해주세요.\n");
-        if(!bePresent(getNoInsuranceCover())) errorText.append("무보험상해 금액을 입력해주세요.\n");
+//        if(!bePresent(getGrade())) errorText.append("등급을 선택해주세요.\n");
+//        if(!bePresent(getStockNumber())) errorText.append("증권번호를 입력해주세요.\n");
+//        if(!bePresent(getFee())) errorText.append("보험료를 입력해주세요.\n");
+//        if(!bePresent(getBikeInsuranceType())) errorText.append("보험용도를 입력해주세요.\n");
+//        if(!bePresent(getType())) errorText.append("보험형태를 입력해주세요.\n");
+//        if(!bePresent(getCompanyName())) errorText.append("보험사를 입력해주세요.\n");
+//        if(!bePresent(getAge())) errorText.append("나이를 입력해주세요.\n");
+//        if(!bePresent(getLiabilityMan())) errorText.append("대인금액을 입력해주세요.\n");
+//        if(!bePresent(getLiabilityCar())) errorText.append("대물금액을 입력해주세요.\n");
+//        if(!bePresent(getLiabilityMan2())) errorText.append("대인2금액을 입력해주세요.\n");
+//        if(!bePresent(getSelfCoverMan())) errorText.append("자손금액을 입력해주세요.\n");
+//        if(!bePresent(getSelfCoverCar())) errorText.append("자차금액을 입력해주세요.\n");
+//        if(!bePresent(getNoInsuranceCover())) errorText.append("무보험상해 금액을 입력해주세요.\n");
     }
 
     public Boolean isAddableBikeInsurance(){
